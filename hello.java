de176f635b8410927fa3df36de0f56188a43998c
JYGim <staryou.kim@gmail.com>
2022-03-25 14:31:04 +0900

    added answer

--- hello.java
+++ hello.java
@@ -1,8 +1,10 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		System.out.println("Hello Git!");
 		System.out.println("login");
-		System.out.println("logout);
+		System.out.println("logout");
+		System.out.println("Register");
+		System.out.println("That's DVCS!");
 	}
 }
 
